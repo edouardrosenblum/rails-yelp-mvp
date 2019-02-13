4f82d005490652c1d46e088eb04cc1eca92fb893
edouard rosenblum <edouardrosenblum@gmail.com>
2019-02-13 15:45:39 +0100

    qsdlk

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+georges = Restaurant.create(name: "georges", address: "1 rue beau", phone_number: "0623456712", category: "french")
+tingtong = Restaurant.create(name: "ting tong", address: "10 rue malar", phone_number: "06236753", category: "chinese")
+trattoria = Restaurant.create(name: "trattoria", address: "20 rue des italiens", phone_number: "12345678", category: "italian")
+lebelge = Restaurant.create(name: "lebelge", address: "1 rue deli", phone_number: "0687686", category: "belgian")
+
+
